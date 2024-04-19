bc76e0a424e4663d0ec286197d6b41a68d473a48
piotr <rtoit007pele@gmail.com>
2024-04-19 22:16:19 +0200

    Refactor form.html and form_check.js for Lab3

--- Lab3/form_check.js
+++ Lab3/form_check.js
@@ -40,6 +40,33 @@ function alterRows(i, e) {
   }
 }
 
+function nextNode(e) {
+  while (e && e.nodeType != 1) {
+    e = e.nextSibling;
+  }
+  return e;
+}
+function prevNode(e) {
+  while (e && e.nodeType != 1) {
+    e = e.previousSibling;
+  }
+  return e;
+}
+function swapRows(b) {
+  let tab = prevNode(b.previousSibling);
+  let tBody = nextNode(tab.firstChild);
+  let lastNode = prevNode(tBody.lastChild);
+  tBody.removeChild(lastNode);
+  let firstNode = nextNode(tBody.firstChild);
+  tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+  if (form.value.length > maxSize)
+    form.value = form.value.substring(0, maxSize);
+  else msg.innerHTML = maxSize - form.value.length;
+}
+
 function validate(form) {
   var result = true;
   if (
